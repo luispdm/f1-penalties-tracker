522838f29e1553ffd2eddc429bc134a091021966
luispdm <17044119+luispdm@users.noreply.github.com>
2023-06-02 01:13:38 +0200

    scrape up to now too

--- scraper/main.go
+++ scraper/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"log"
+	"net/url"
 	"regexp"
 	"time"
 
@@ -13,17 +14,22 @@ const (
 	linksSelector = "li.document-row a"
 	offenceRe     = `(Offence|Decision) - Car [0-9]{1,2} - (PU elements|RNC Changes)`
 	newElemsRe    = `New (PU elements|RNCs)`
+	upToNowRe     = `(PU elements|RNCs) used per driver up to now`
 	ua            = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36"
+	// up to now needed in order to be consistent: when offences occur after up to now is published,
+	// parts counter can't be increased because natural language is not processed
+	// RNC issues | allocations ?
 )
 
 func main() {
 	oRe := regexp.MustCompile(offenceRe)
 	nERe := regexp.MustCompile(newElemsRe)
+	uTNRe := regexp.MustCompile(upToNowRe)
 	c := colly.NewCollector(colly.UserAgent(ua), colly.AllowedDomains("www.fia.com"))
 	err := c.Limit(&colly.LimitRule{
-		Delay:       10 * time.Second,
+		Delay:        10 * time.Second,
 		DomainRegexp: `fia\.com`,
-		Parallelism: 1,
+		Parallelism:  1,
 	})
 	if err != nil {
 		log.Fatal(err)
@@ -32,9 +38,23 @@ func main() {
 	c.OnHTML(linksSelector, func(h *colly.HTMLElement) {
 		link := h.Attr("href")
 		if oRe.MatchString(link) {
-			log.Printf("downloading offence file %s", link)
+			parsed, err := url.Parse(link)
+			if err != nil {
+				log.Fatal(err)
+			}
+			log.Printf("found offence file %s", parsed)
 		} else if nERe.MatchString(link) {
-			log.Printf("downloading new elements file %s", link)
+			parsed, err := url.Parse(link)
+			if err != nil {
+				log.Fatal(err)
+			}
+			log.Printf("found new elements file %s", parsed)
+		} else if uTNRe.MatchString(link) {
+			parsed, err := url.Parse(link)
+			if err != nil {
+				log.Fatal(err)
+			}
+			log.Printf("found up to now file %s", parsed)
 		}
 	})
 	err = c.Visit(here)
